d199a522d5c56b7184c93fdcf82c3fc3c59d33e0
Gilmer Contreras <adgdiux@gmail.com>
2023-11-04 06:37:09 -0400

    SEO

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 // DOMAINS
 /**
- base_url: 'https://monitor.simids.com.co/api'
+ base_url: 'https://elias360construction.com/api'
  base_url: 'http://localhost:3000/api'
  */
 
 export const environment = {
   production: false,
-  base_url: 'http://localhost:3000/api'
+  base_url: 'https://elias360construction.com/api'
 };
 
 /*
